7bdbeef2b71ddb39064893480674594e519399e2
Nata1609 <ulbajchik@gmail.com>
2020-11-17 19:32:56 +0200

    My 2nd homework, CheckboxDemo

---
+++ Autotestai/Page/CheckboxDemoPage.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autotestai.Page
+{
+    public class CheckboxDemoPage
+    {
+        private static IWebDriver _driver;
+
+        private IWebElement firstCheckbox => _driver.FindElement(By.Id("isAgeSelected"));
+        private IWebElement text => _driver.FindElement(By.Id("txtAge"));
+        private IReadOnlyCollection<IWebElement> multipleCheckboxList => _driver.FindElements(By.CssSelector(".cb1-element"));
+        private string checkButton => _driver.FindElement(By.Id("check1")).GetAttribute("value");
+        private string checkButton2 => _driver.FindElement(By.Id("check1")).GetAttribute("value");
+
+
+        public CheckboxDemoPage(IWebDriver webdriver) => _driver = webdriver;
+
+        public void ClickOnThisCheckbox()
+        {
+            firstCheckbox.Click();
+        }
+
+        public void CheckResultIfSuccess()
+        {
+           Assert.IsTrue(text.Text.Equals("Success - Check box is checked"));
+        }
+
+        public void CheckIfClickOnThisCheckboxNotSelected()
+        {
+            if (firstCheckbox.Selected)
+             ClickOnThisCheckbox(); 
+        }
+
+        public void ClickOnAllMultipleCheckboxes()
+        {
+            foreach (IWebElement element in multipleCheckboxList)
+            {
+                element.Click();
+
+            }
+        }
+
+        public void CheckResultIfUncheckAllButtonsAppear(string text)
+        {
+            Assert.That(checkButton, Is.EqualTo(text));
+        }
+
+        public void UnclickOnAllMultipleCheckboxes() 
+        {
+            ClickOnAllMultipleCheckboxes();
+            ClickOnAllMultipleCheckboxes();
+        }
+
+        public void CheckResultIfCheckAllButtonAppear(string text)
+        {
+            Assert.That(checkButton2, Is.EqualTo(text));
+        }
+
+
+
+
+    }
+
+}
---
+++ Autotestai/Test/CheckboxDemoTest.cs
@@ -0,0 +1,64 @@
+﻿using Autotestai.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autotestai.Test
+{
+    public class CheckboxDemoTest
+    {
+
+        private static IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/basic-checkbox-demo.html");
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public static void TearDown()
+        {
+            //_driver.Quit();
+
+        }
+
+        [Test]
+        public void CheckboxTestFirstBlock()
+        {
+            CheckboxDemoPage page = new CheckboxDemoPage(_driver);
+
+            page.ClickOnThisCheckbox();
+            page.CheckResultIfSuccess();
+
+        }
+
+        [TestCase("Uncheck All")]
+        [TestCase("Check All")]
+
+        public void MultipleCheckboxTest(string msg)
+        {
+            CheckboxDemoPage page = new CheckboxDemoPage(webdriver: _driver);
+
+            page.CheckIfClickOnThisCheckboxNotSelected();
+            page.ClickOnAllMultipleCheckboxes();
+            page.CheckResultIfUncheckAllButtonsAppear(msg);
+            page.UnclickOnAllMultipleCheckboxes();
+            page.CheckResultIfCheckAllButtonAppear(msg);
+
+
+        }
+
+
+
+    }
+}
